ea4a1f9f9496fc2bd80610d2c5793feb66623312
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-30 15:44:15 +0100

    [add]: index.php/head.php/bootstrap (cdn)

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+$todo_tasks = [
+
+  // [
+  //   text => 'Tidy the room up',
+  //   isDone => false
+  // ],
+  // [
+  //   text => 'Tidy the code up',
+  //   isDone => false
+  // ],
+  // [
+  //   text => 'Get dressed',
+  //   isDone => false
+  // ],
+  // [
+  //   text => 'Wash yourself',
+  //   isDone => false
+  // ]
+
+  [
+    "text" => 'Tidy the room up',
+    "isDone" => false
+  ],
+  [
+    "text" => 'Tidy the code up',
+    "isDone" => false
+  ],
+  [
+    "text" => 'Get dressed',
+    "isDone" => false
+  ],
+  [
+    "text" => 'Wash yourself',
+    "isDone" => false
+  ]
+
+];
+
+require_once __DIR__ . '/partials/head.php';
+
+?>
+
+
+<body>
+
+<div class="container rounded-3 my-3">
+  <h2>Hello World!</h2>
+</div>
+  
+</body>
+</html>
