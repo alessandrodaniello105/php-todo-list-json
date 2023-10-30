a653a5fe1a180d5ec0a66681e40383e5b4b2e98a
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-30 16:44:29 +0100

    [edit]: $list is now $tasks_list

--- server.php
+++ server.php
@@ -2,8 +2,10 @@
 
 $todo_tasks_string = file_get_contents('todo-tasks.json');
 
-$list = json_decode($todo_tasks_string, true);
+$tasks_list = json_decode($todo_tasks_string, true);
 
-echo json_encode($list);
+
+
+echo json_encode($tasks_list);
 
 ?>
