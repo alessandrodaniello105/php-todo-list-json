1641ba4a06a0bd1b1061dd433fb6f8ad65f93463
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-31 11:46:46 +0100

    [add]: remove+toggle task in server.php

--- server.php
+++ server.php
@@ -4,22 +4,43 @@ $todo_tasks_string = file_get_contents('todo-tasks.json');
 
 $tasks_list = json_decode($todo_tasks_string, true);
 
+
+// ADD TASK
 if( isset($_POST['newTaskElement']) ) {
+
   $newTaskEl = json_decode($_POST['newTaskElement']);
 
   $tasks_list[] = $newTaskEl;
+
   file_put_contents( 'todo-tasks.json', json_encode($tasks_list) );
 
 }
 
+// REMOVE TASK
 if ( isset($_POST['idTask']) ) {
+  
   $taskToRemove = $_POST['idTask'];
 
-  array_splice($tasks_list, $taskToRemove, 1);
+  if ($tasks_list[$taskToRemove]['isDone'] == true) {
+    array_splice($tasks_list, $taskToRemove, 1);
+  } 
 
   file_put_contents( 'todo-tasks.json', json_encode($tasks_list) );
 
 }
+
+
+// TOGGLE DONE TASK
+if ( isset($_POST['idDone'])) {
+
+  $idDone = $_POST['idDone'];
+
+  $tasks_list[$idDone]['isDone'] = !$tasks_list[$idDone]['isDone'];
+
+  file_put_contents( 'todo-tasks.json', json_encode($tasks_list) );
+
+}
+
 
 header('Content-Type: application/json');
 echo json_encode($tasks_list);
