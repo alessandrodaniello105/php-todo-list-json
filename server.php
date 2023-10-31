263ebdeb97fb6a53b259f2b4f4d1d77e69735b5f
alessandrodaniello105 <kanymsevement@gmail.com>
2023-10-31 10:55:14 +0100

    [add]: remove task logic into server/json

--- server.php
+++ server.php
@@ -11,7 +11,15 @@ if( isset($_POST['newTaskElement']) ) {
   file_put_contents( 'todo-tasks.json', json_encode($tasks_list) );
 
 }
-file_put_contents( 'todo-tasks.json', json_encode($tasks_list) );
+
+if ( isset($_POST['idTask']) ) {
+  $taskToRemove = $_POST['idTask'];
+
+  array_splice($tasks_list, $taskToRemove, 1);
+
+  file_put_contents( 'todo-tasks.json', json_encode($tasks_list) );
+
+}
 
 header('Content-Type: application/json');
 echo json_encode($tasks_list);
